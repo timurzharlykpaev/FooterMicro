09e11be37aeac006b2608e9310ca8ac15f6ac3b7
Timur <84371383+timurzharlykpaev@users.noreply.github.com>
2024-08-27 10:07:37 -0400

    init

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Footer Component</p>
+    </footer>
+  );
+};
+
+export default Footer;
